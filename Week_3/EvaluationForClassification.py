215169547d9672edf7fa54859ba65d8bcb6a84a1
Shatha Naami <shatha@things.sa>
2022-02-18 16:21:46 +0200

    EvaluationForClassification.py implement

---
+++ Week_3/EvaluationForClassification.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+# import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_digits
+
+
+# /Users/shathanaami/PycharmProjects/AppliedMachineLearningInPython/venv/bin/activate
